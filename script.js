76c54a8e4e314020b9b412e4c7b683abe9c73583
FaizulOsman <faizulosman@gmail.com>
2022-08-19 17:44:43 +0100

    final commit

--- script.js
+++ script.js
@@ -1,42 +1,37 @@
-// /* Frist Way to generate random number */
-// function getPin() {
-//     const pin = generatePin()
-//     const pinStr = pin + ''
-//     if (pinStr.length == 4) {
-//         return pin
-//     } else {
-//         return getPin()
-//     }
-// }
-
-// function generatePin() {
-//     const random = Math.round(Math.random() * 10000)
-//     return random
-// }
-
-// document.getElementById('generate-btn').addEventListener('click', function () {
-//     const generateFormControl = document.getElementById('generate-form-control')
-//     generateFormControl.value = getPin()
-// })
-
-
-
-
-/* Second Way to generate random number */
-document.getElementById('generate-btn').addEventListener('click', function () {
-    const generate = Math.round(Math.random() * 10000)
-    let pin;
-    if (generate / 1000 > 1) {
-        pin = generate
-    } else if (generate / 100 > 1) {
-        pin = generate * 10
+/* Frist Way to generate random number */
+function getPin() {
+    const pin = Math.round(Math.random() * 10000)
+    const pinStr = pin + ''
+    if (pinStr.length == 4) {
+        return pin
     } else {
-        pin = generate * 100
+        return getPin()
     }
+}
 
+document.getElementById('generate-btn').addEventListener('click', function () {
     const generateFormControl = document.getElementById('generate-form-control')
-    generateFormControl.value = pin
+    generateFormControl.value = getPin()
 })
+
+
+
+
+// /* Second Way to generate random number */
+// document.getElementById('generate-btn').addEventListener('click', function () {
+//     const generate = Math.round(Math.random() * 10000)
+//     let pin;
+//     if (generate / 1000 > 1) {
+//         pin = generate
+//     } else if (generate / 100 > 1) {
+//         pin = generate * 10
+//     } else {
+//         pin = generate * 100
+//     }
+
+//     const generateFormControl = document.getElementById('generate-form-control')
+//     generateFormControl.value = pin
+// })
 
 
 
@@ -44,12 +39,14 @@ document.getElementById('generate-btn').addEventListener('click', function () {
 
 /* Calculate = Verify your pin section */
 document.getElementById('calculate').addEventListener('click', function (event) {
-    const value = event.target.innerText
+    const value = event.target.innerText                                        // See This Code
     const submitFormControl = document.getElementById('submit-form-control')
-    if (value == 'C') {
-        submitFormControl.value = ''
-    } else if (value == '<') {
-        submitFormControl.value = submitFormControl.value.slice(0, -1)
+    if (isNaN(value)) {
+        if (value == 'C') {
+            submitFormControl.value = ''
+        } else if (value == '<') {
+            submitFormControl.value = submitFormControl.value.slice(0, -1)
+        }
     } else {
         submitFormControl.value += value
     }
@@ -74,8 +71,8 @@ document.getElementById('submit-btn').addEventListener('click', function () {
         notifyYes.style.display = 'block'
         notifyNot.style.display = 'none'
         tryLeft.style.display = 'none'
-        document.getElementById('submit-form-control').value = ''
         document.getElementById('generate-form-control').value = ''
+        document.getElementById('submit-form-control').value = ''
     } else {
         notifyNot.style.display = 'block'
         notifyYes.style.display = 'none'
